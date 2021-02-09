1da340a6976122c85a230a6c8394e360267ef6ec
Black and White Penguin <balckandwhitepenguin12@gmail.com>
2021-02-09 13:29:54 +0545

    Moved to git
    Project moved to github

---
+++ register.php
@@ -0,0 +1,142 @@
+<?php
+    include("includes/config.php");
+
+    include("includes/classes/Account.php");
+    include("includes/classes/Constants.php");
+
+    $account = new Account($con);
+
+
+
+    include("includes/handlers/register-handler.php");
+    include("includes/handlers/login-handler.php");
+
+
+    function getInputValue($name) {
+        if(isset($_POST[$name])) {
+            echo $_POST[$name];
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Slotify</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/register.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="assets/js/register.js"></script>
+</head>
+<body>
+    <?php 
+        if(isset($_POST['registerButton'])) {
+            echo '<script>
+                $(document).ready(function () {
+
+                    $("#loginForm").hide();
+                    $("#registerForm").show();
+                });
+             </script>';
+        } else {
+            echo '<script>
+                $(document).ready(function () { 
+                    $("#loginForm").show();
+                    $("#registerForm").hide();
+                });
+            </script>';
+        }
+    ?>
+
+    <div id="background">
+        <div id="loginContainer">
+            <div id="inputContainer">
+                <form action="register.php" id="loginForm" method="POST">
+                    <h2>Login to your account</h2>
+                    <p>
+                    <?php echo $account->getError(Constants::$loginFailed); ?>
+                    <label for="loginUsername">Username</label>
+                    <input type="text" id="loginUsername" name="loginUsername" placeholder="e.g. Sebastian" value="<?php getInputValue('loginUsername') ?>"required>
+                    </p>
+                    <p>
+                    <label for="loginPassword">Passwword</label>
+                    <input type="password" id="loginPassword" name="loginPassword" placeholder="Your password" required>
+                    </p>
+
+                    <button type="submit" name="loginButton">Log in</button>
+
+                    <div class="hasAccountText">
+                            <span id="hideLogin">Don`t have an account yet? Sign up here.</span>
+                    </div>
+                </form>
+
+
+                <form action="register.php" id="registerForm" method="POST">
+                    <h2>Create your free account</h2>
+                    <p>
+                    <?php echo $account->getError(Constants::$usernameCharacters); ?>
+                    <?php echo $account->getError(Constants::$usernameTaken); ?>
+                    <label for="username">Username</label>
+                    <input type="text" id="username" name="username" placeholder="e.g. Sebastian Ronaldo" value="<?php getInputValue('username') ?>" required>
+                    </p>
+                    <p>
+                    <?php echo $account->getError(Constants::$firstNameCharacters); ?>
+                    <label for="firstame">First name</label>
+                    <input type="text" id="firstName" name="firstName" placeholder="e.g. Sebastian" value="<?php getInputValue('firstName') ?>" required>
+                    </p>
+                    <p>
+                    <?php echo $account->getError(Constants::$lastNameCharacters); ?>
+                    <label for="lastName">Last name</label>
+                    <input type="text" id="lastName" name="lastName" placeholder="e.g. Ronaldo" value="<?php getInputValue('lastName') ?>" required>
+                    </p>
+                    <p>
+                    <?php echo $account->getError(Constants::$emailsDoNotMatch); ?>
+                    <?php echo $account->getError(Constants::$emailInvalid); ?>
+                    <?php echo $account->getError(Constants::$emailTaken); ?>
+                    <label for="email">Email</label>
+                    <input type="email" id="email" name="email" placeholder="e.g. adc@gmail.com" value="<?php getInputValue('email') ?>" required>
+                    </p>
+                    <p>
+                    <label for="email2">Confirm email</label>
+                    <input type="email" id="email2" name="email2" placeholder="e.g. abc@gmail.com" value="<?php getInputValue('email2') ?>" required>
+                    </p>
+                    <p>
+                    <?php echo $account->getError(Constants::$passwordsDoNotMatch); ?>
+                    <?php echo $account->getError(Constants::$passwordNotAlphanumeric); ?>
+                    <?php echo $account->getError(Constants::$passwordCharacters); ?>
+                    <label for="password">Passwword</label>
+                    <input type="password" id="password" name="password" placeholder="Your password" required>
+                    </p>
+                    <p>
+                    <label for="password2">Confirm passwword</label>
+                    <input type="password" id="password2" name="password2" placeholder="Your password"required>
+                    </p>
+
+                    <button type="submit" name="registerButton">Sign Up</button>
+                    <div class="hasAccountText">
+                            <span id="hideRegister">Already have an account? Log in here</span>
+                    </div>
+                </form>
+            </div>
+
+            <div id="loginText">
+                <h1>
+                    Get great music, right now
+                </h1>
+                <h2>
+                    Listen to loads of songs for free
+                </h2>
+
+                <ul>
+                    <li>Discover music you`ll fall in love with</li>
+                    <li>Create your own playlists</li>
+                    <li>Follow artists to keep up to date</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
